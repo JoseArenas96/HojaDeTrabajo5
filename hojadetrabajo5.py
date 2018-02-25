604c98acfb58c385c2481fc24ccaea07a07f8d87
sevic69 <sebdeveloper6952@gmail.com>
2018-02-25 11:53:57 -0600

    Agregar parametros de simulacion.

--- hojadetrabajo5.py
+++ hojadetrabajo5.py
@@ -1,1 +1,18 @@
 import simpy
+from random import seed, randint, expovariate
+
+# parametros de simulacion
+random_seed = 12345
+numero_cpus = 1
+numero_procesos = 10
+cpu_instrucciones_ciclo = 3
+cantidad_memoria = 100
+intervalo_llegada_procesos = 10
+
+
+# crear Environment de simpy y Resources
+seed(random_seed)
+env = simpy.Environment()
+cpu = simpy.Resource(env, 1)
+mem = simpy.Container(env, cantidad_memoria, cantidad_memoria)
+env.run()
